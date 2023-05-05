b3695de8f50ff347cb99af841131b78a8b297f3d
LStan <LStan@users.noreply.github.com>
2023-05-05 17:52:54 +0700

    week2 project init

---
+++ Week02/BallotProject/scripts/Ballot.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+import { Ballot__factory } from "../typechain-types";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+
+async function main() {
+  console.log("Deploying Ballot contract");
+  console.log("Proposals: ");
+  PROPOSALS.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
+  // TODO
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
