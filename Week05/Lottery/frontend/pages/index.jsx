1f956ce5bc4d487ba67a65d0a670d5fe414988ae
LStan <LStan@users.noreply.github.com>
2023-06-04 03:16:57 +0700

    Move componets to separate files

--- Week05/Lottery/frontend/pages/index.jsx
+++ Week05/Lottery/frontend/pages/index.jsx
@@ -1,11 +1,19 @@
 import styles from "../styles/Home.module.css";
-import InstructionsComponent from "../components/InstructionsComponent";
+import PageBody from "../components/PageBody";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <InstructionsComponent></InstructionsComponent>
+        <div className={styles.container}>
+          <header className={styles.header_container}>
+            <h1>Lottery dApp</h1>
+          </header>
+          <div className={styles.buttons_container}>
+            <PageBody></PageBody>
+          </div>
+          <div className={styles.footer}>Group 4</div>
+        </div>
       </main>
     </div>
   );
