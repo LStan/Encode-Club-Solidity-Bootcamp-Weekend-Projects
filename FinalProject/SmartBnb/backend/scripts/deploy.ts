5c530edd1390801e76cad8660a7f38ce88bbc699
LStan <LStan@users.noreply.github.com>
2023-06-10 18:49:13 +0700

    Add contract code

--- FinalProject/SmartBnb/backend/scripts/deploy.ts
+++ FinalProject/SmartBnb/backend/scripts/deploy.ts
@@ -1,8 +1,12 @@
 import { ethers } from "hardhat";
 
+const LISTING_FEE = 0.0001;
+
 async function main() {
   const Contract = await ethers.getContractFactory("SmartBnb");
-  const contract = await Contract.deploy();
+  const contract = await Contract.deploy(
+    ethers.utils.parseEther(LISTING_FEE.toFixed(18))
+  );
 
   await contract.deployed();
 
