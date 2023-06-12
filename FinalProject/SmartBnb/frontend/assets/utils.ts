1f4954e918e57c4bb14d739cbab4ae9a73602bce
LStan <LStan@users.noreply.github.com>
2023-06-12 14:24:53 +0700

    Fix wrong Link import

--- FinalProject/SmartBnb/frontend/assets/utils.ts
+++ FinalProject/SmartBnb/frontend/assets/utils.ts
@@ -1,39 +1,48 @@
 import contractJson from "./SmartBnb.json";
-import { localhost, sepolia, polygonMumbai, goerli } from "wagmi/chains";
+import { localhost, sepolia, polygonMumbai, goerli, Chain } from "wagmi/chains";
 import { configureChains } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { ethers } from "ethers";
 
-const SMARTBNB_CONTRACT_ADDRESS_LOCALHOST = "0x40d3989CF95885f6456aCe44beC69Ac198Eb06F9";
+const SMARTBNB_CONTRACT_ADDRESS_LOCALHOST =
+  "0x40d3989CF95885f6456aCe44beC69Ac198Eb06F9";
 
-const SMARTBNB_CONTRACT_ADDRESS_GOERLI = "0x5712DE56c5E00CE7223D6554617700B988DA1E5D";
+const SMARTBNB_CONTRACT_ADDRESS_GOERLI =
+  "0x5712DE56c5E00CE7223D6554617700B988DA1E5D";
 
 const SMARTBNB_CONTRACT_ADDRESS_MUMBAI = "";
 
 const SMARTBNB_CONTRACT_ADDRESS_SEPOLIA = "";
 
-export function getSmartBnbContract(signer, chain) {
+export function getSmartBnbContract(
+  signerOrProvider: ethers.providers.Provider | ethers.Signer,
+  chain: Chain
+) {
   switch (chain.network) {
     case "localhost":
       return new ethers.Contract(
         SMARTBNB_CONTRACT_ADDRESS_LOCALHOST,
         contractJson.abi,
-        signer
+        signerOrProvider
       );
     case "goerli":
-      return new ethers.Contract(SMARTBNB_CONTRACT_ADDRESS_GOERLI, contractJson.abi, signer);
+      return new ethers.Contract(
+        SMARTBNB_CONTRACT_ADDRESS_GOERLI,
+        contractJson.abi,
+        signerOrProvider
+      );
     case "sepolia":
       return new ethers.Contract(
         SMARTBNB_CONTRACT_ADDRESS_MUMBAI,
         contractJson.abi,
-        signer
+        signerOrProvider
       );
     case "maticmum":
       return new ethers.Contract(
         SMARTBNB_CONTRACT_ADDRESS_SEPOLIA,
         contractJson.abi,
-        signer
+        signerOrProvider
       );
     default:
       throw new Error("Unknown network!");
